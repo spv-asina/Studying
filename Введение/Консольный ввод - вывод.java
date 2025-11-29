e5ccca42a0ae8c00baeb7a98ac43e985034badf2
spv_asina <spv.asinagi@gmail.com>
2025-11-29 18:04:02 +0300

    Консольный ввод - вывод

---
+++ Введение/Консольный ввод - вывод.java
@@ -0,0 +1,6 @@
+void main(){
+    java.util.Scanner input = new java.util.Scanner(System.in);
+    String name = input.nextLine();
+    System.out.print("Привет, ");
+    System.out.println(name);
+}
