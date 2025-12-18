cd370ac9ebd4f00507bdd596397ca04ca05f53a0
spv_asina <spv.asinagi@gmail.com>
2025-12-18 18:11:54 +0300

    Точка входа

---
+++ main.cpp
@@ -0,0 +1,20 @@
+﻿#include "tests.h"
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    try {
+        test_assignment();
+        test_different_args();
+        test_errors();
+        test_threads();
+        test_defaults();
+
+        return 0;
+    }
+    catch (const exception& e) {
+        cerr << "\nОшибка: " << e.what() << endl;
+        return 1;
+    }
+}
